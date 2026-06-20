439c66bb683b1831fb48db2a5a467caf94843396
ASGoncharov-avl <alexandrgoncharov2015@ya.ru>
2026-06-20 17:53:33 +0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.0.106:8000';
+const API_URL = 'http://localhost:8000';
 
 export const api = {
   createPlayer: async (name: string) => {
